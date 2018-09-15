2fcfe6b393de5f0d639ed6e3249366eab0b8c3f9
BenTalagan <ben_talagan@github.nospam.com>
2018-09-15 20:59:45 +0200

    Fixing Eldamar GlaemUnicode wrong name, and rebuilding unicode transitionnal fonts

--- fonts/src/build.rb
+++ fonts/src/build.rb
@@ -6,15 +6,15 @@ Dir.chdir(__dir__)
 puts "Building fonts, autohinting them with ttfautohint."
 puts "We do this because they are unicode fonts with no latin part and ttfautohint crashes on font squirrel without the -s option"
 
-%x(ttfautohint ./ttfs_exports/sarati-eldamar-rtlb-glaemscrafu-noautohint.ttf       ../build/ttfs/sarati-eldamar-rtlb-glaemscrafu.ttf)
-%x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-annatar-glaemunicode.ttf -s)
-%x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-annatar-glaemunicode.ttf -s)
-%x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-bold-noautohint.ttf     ../build/ttfs/tengwar-annatar-glaemunicode-bold.ttf -s)
-%x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-italic-noautohint.ttf   ../build/ttfs/tengwar-annatar-glaemunicode-italic.ttf -s)
-%x(ttfautohint ./ttfs_exports/tengwar-eldamar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-eldamar-glaemunicode.ttf -s)
-%x(ttfautohint ./ttfs_exports/tengwar-elfica-glaemunicode-noautohint.ttf           ../build/ttfs/tengwar-elfica-glaemunicode.ttf -s)
-%x(ttfautohint ./ttfs_exports/tengwar-parmaite-glaemunicode-noautohint.ttf         ../build/ttfs/tengwar-parmaite-glaemunicode.ttf -s)
-%x(ttfautohint ./ttfs_exports/tengwar-sindarin-glaemunicode-noautohint.ttf         ../build/ttfs/tengwar-sindarin-glaemunicode.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/sarati-eldamar-rtlb-glaemscrafu-noautohint.ttf       ../build/ttfs/sarati-eldamar-rtlb-glaemscrafu.ttf)
+puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-annatar-glaemunicode.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-annatar-glaemunicode.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-bold-noautohint.ttf     ../build/ttfs/tengwar-annatar-glaemunicode-bold.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-italic-noautohint.ttf   ../build/ttfs/tengwar-annatar-glaemunicode-italic.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/tengwar-eldamar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-eldamar-glaemunicode.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/tengwar-elfica-glaemunicode-noautohint.ttf           ../build/ttfs/tengwar-elfica-glaemunicode.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/tengwar-parmaite-glaemunicode-noautohint.ttf         ../build/ttfs/tengwar-parmaite-glaemunicode.ttf -s)
+puts %x(ttfautohint ./ttfs_exports/tengwar-sindarin-glaemunicode-noautohint.ttf         ../build/ttfs/tengwar-sindarin-glaemunicode.ttf -s)
 
 puts "... now, convert files in build/ttfs to build/webs using font squirrel."
 
