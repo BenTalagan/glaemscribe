c87f10cbe3133e2f650ac5810f123258c656b579
BenTalagan <ben_talagan@github.nospam.com>
2019-02-07 21:58:17 +0100

    Add some more features to autohinted script

--- fonts/src/build-autohinted-fonts.rb
+++ fonts/src/build-autohinted-fonts.rb
@@ -6,16 +6,57 @@ Dir.chdir(__dir__)
 puts "Building fonts, autohinting them with ttfautohint."
 puts "We do this because they are unicode fonts with no latin part and ttfautohint crashes on font squirrel without the -s option"
 
-puts %x(ttfautohint ./ttfs_exports/sarati-eldamar-rtlb-glaemscrafu-noautohint.ttf       ../build/ttfs/sarati-eldamar-rtlb-glaemscrafu.ttf)
-puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-annatar-glaemunicode.ttf -s)
-puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-annatar-glaemunicode.ttf -s)
-puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-bold-noautohint.ttf     ../build/ttfs/tengwar-annatar-glaemunicode-bold.ttf -s)
-puts %x(ttfautohint ./ttfs_exports/tengwar-annatar-glaemunicode-italic-noautohint.ttf   ../build/ttfs/tengwar-annatar-glaemunicode-italic.ttf -s)
-puts %x(ttfautohint ./ttfs_exports/tengwar-eldamar-glaemunicode-noautohint.ttf          ../build/ttfs/tengwar-eldamar-glaemunicode.ttf -s)
-puts %x(ttfautohint ./ttfs_exports/tengwar-elfica-glaemunicode-noautohint.ttf           ../build/ttfs/tengwar-elfica-glaemunicode.ttf -s)
-puts %x(ttfautohint ./ttfs_exports/tengwar-parmaite-glaemunicode-noautohint.ttf         ../build/ttfs/tengwar-parmaite-glaemunicode.ttf -s)
-puts %x(ttfautohint ./ttfs_exports/tengwar-sindarin-glaemunicode-noautohint.ttf         ../build/ttfs/tengwar-sindarin-glaemunicode.ttf -s)
+def build_font(font_name)
+  puts "Building #{font_name}"
+  puts %x(ttfautohint ./ttfs_exports/#{font_name}-noautohint.ttf          ../build/ttfs/#{font_name}.ttf -s)
+  puts "Done."
+end
+
+available_fonts = [
+  "sarati-eldamar-rtlb-glaemscrafu",
+  "tengwar-annatar-glaemunicode",
+  "tengwar-annatar-glaemunicode-bold",
+  "tengwar-annatar-glaemunicode-italic",
+  "tengwar-eldamar-glaemunicode",
+  "tengwar-elfica-glaemunicode",
+  "tengwar-parmaite-glaemunicode",
+  "tengwar-sindarin-glaemunicode",
+]
+
+should_build_all = false
+ARGV.each { |a|
+  if a == "--all"
+    should_build_all = true
+  end
+}
+
+if should_build_all
+  puts "Building all available fonts."
+  available_fonts.each{ |f|
+    build_font(f)
+  }
+else
+  missing_font = false
+  has_fonts    = false
+  ARGV.each { |a|
+    if !available_fonts.include? a
+      puts "*** Illegal font name #{a}"
+      missing_font = true
+    else
+      has_fonts = true
+    end
+  }
+  exit(2) if missing_font
+  
+  if !has_fonts
+    puts "*** no font to build! Pass font names or --all"
+    exit(3)
+  end
+  
+  ARGV.each { |a|
+    build_font(a)
+  }
+  
+end
 
 puts "... now, convert files in build/ttfs to build/webs using font squirrel."
-
-puts "Done."
