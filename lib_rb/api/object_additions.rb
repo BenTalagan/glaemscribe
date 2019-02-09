b18d0f11e2c9dc3e037cb1099d85fde2e326f3c0
BenTalagan <ben_talagan@github.nospam.com>
2019-02-09 17:15:53 +0100

    Fixed deep cloning for ruby 2.6

--- lib_rb/api/object_additions.rb
+++ lib_rb/api/object_additions.rb
@@ -1,7 +1,9 @@
 # From http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-list/43424
 class Object
+=begin
   def deep_clone
     return @deep_cloning_obj if @deep_cloning
+      
     @deep_cloning_obj = clone
     @deep_cloning_obj.instance_variables.each do |var|
       val = @deep_cloning_obj.instance_variable_get(var)
@@ -19,4 +21,24 @@ class Object
     @deep_cloning_obj = nil
     deep_cloning_obj
   end
+=end
+  
+  def deep_clone(cache={})
+    return cache[self] if cache.key?(self)
+
+    copy = clone()
+    cache[self] = copy
+
+    copy.instance_variables.each do |var|
+      val = instance_variable_get(var)
+      begin
+        val = val.deep_clone(cache)
+      rescue TypeError
+        next
+      end
+      copy.instance_variable_set(var, val)
+    end
+
+    return copy
+  end  
 end
