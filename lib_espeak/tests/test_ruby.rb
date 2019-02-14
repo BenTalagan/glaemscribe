7abd93b318e470dd46c8624cf0645b8844ecf523
BenTalagan <ben_talagan@github.nospam.com>
2019-02-14 14:31:08 +0100

    espeakng update

--- lib_espeak/tests/test_ruby.rb
+++ lib_espeak/tests/test_ruby.rb
@@ -3,8 +3,8 @@ require "mini_racer"
 
 require File.dirname(__FILE__) + "/../../lib_rb/glaemscribe.rb"
 
-puts Glaemscribe::API::TTS.ipa("Hi, my name is toto!","en")
-puts Glaemscribe::API::TTS.ipa("Hi, my \n name \"is\" toto!","en")["ipa"]
-puts Glaemscribe::API::TTS.ipa("In a hole in the ground, there lived a hobbit.","en")
+puts Glaemscribe::API::TTS.ipa("Hi, my name is toto!","en",false)
+puts Glaemscribe::API::TTS.ipa("Hi, my \n name \"is\" toto!","en",false)
+puts Glaemscribe::API::TTS.ipa("In a hole in the ground, there lived a hobbit.","en",false)
 
-puts Glaemscribe::API::TTS.ipa("Hi, my name is toto! " * 1000,"en")
+puts Glaemscribe::API::TTS.ipa("Hi, my name is toto! " * 1000,"en",false)
