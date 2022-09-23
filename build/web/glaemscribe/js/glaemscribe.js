a641e69948e9989b8f0bd80e0287547201500484
BenTalagan <ben_talagan@github.nospam.com>
2022-09-23 16:08:05 +0200

    Rebuild files accordingly, preparing 1.3.0

--- build/web/glaemscribe/js/glaemscribe.js
+++ build/web/glaemscribe/js/glaemscribe.js
@@ -19,7 +19,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Version : 1.2.1
+Version : 1.3.0
 */
 
 "use strict";
